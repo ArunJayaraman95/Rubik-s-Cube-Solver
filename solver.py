8abdc434b811912bad15512d0375796d73978ff7
Arun Jayaraman <riparun.aj@gmail.com>
2022-01-25 12:59:42 -0500

    All base moves. Debugging

--- solver.py
+++ solver.py
@@ -6,13 +6,15 @@ from timeit import default_timer as timer
 # Define colors
 r, w, b, o, y, g = "R", "W", "B", "O", "Y", "G"
 
+def cta(s):
+    return [i for i in s]
 # Define cube faces
-front = [b]*9
-back = [g]*9
-left = [r]*9
-right = [o]*9
-up = [w]*9
-down = [y]*9
+front = cta("BBWBBWBBW")
+back =  cta("GGYGGYGGY")
+left =  cta("RRRRRRRRR")
+right = cta("OOOOOOOOO")
+up =    cta("WWGWWGWWG")
+down =  cta("YYBYYBYYB")
 
 def display():
     print(f"\n       {back[0]} {back[1]} {back[2]}\n       {back[3]} {back[4]} {back[5]}\n       {back[6]} {back[7]} {back[8]}\n")
@@ -29,6 +31,7 @@ def R():
     down[2], down[5], down[8] = back[2], back[5], back[8]
     back[2], back[5], back[8] = up[2], up[5], up[8]
     up[2], up[5], up[8] = t1, t2, t3
+
     t = right[0]
     right[0] = right[3]
     right[3] = right[6]
@@ -46,24 +49,101 @@ def L():
     up[0], up[3], up[6] = back[0], back[3], back[6]
     back[0], back[3], back[6] = down[0], down[3], down[6]
     down[0], down[3], down[6] = t1, t2, t3
-    t = right[0]
-    right[0] = right[3]
-    right[3] = right[6]
-    right[6] = right[7]
-    right[7] = right[8]
-    right[8] = right[5]
-    right[5] = right[2]
-    right[2] = right[1]
-    right[1] = t
+
+    t = left[0]
+    left[0] = left[3]
+    left[3] = left[6]
+    left[6] = left[7]
+    left[7] = left[8]
+    left[8] = left[5]
+    left[5] = left[2]
+    left[2] = left[1]
+    left[1] = t
+
+
+def U():
+    t1, t2, t3 = front[0], front[1], front[2]
+    front[0], front[1], front[2] = right[0], right[3], right[6]
+    right[0], right[3], right[6] = back[8], back[7], back[6]
+    back[8], back[7], back[6] = left[2], left[5], left[8]
+    left[2], left[5], left[8] = t1, t2, t3
+
+    t = up[0]
+    up[0] = up[3]
+    up[3] = up[6]
+    up[6] = up[7]
+    up[7] = up[8]
+    up[8] = up[5]
+    up[5] = up[2]
+    up[2] = up[1]
+    up[1] = t
+
+
+def D():
+    t1, t2, t3 = front[6], front[7], front[8]
+    front[6], front[7], front[8] = left[0], left[3], left[6]
+    left[0], left[3], left[6] = back[0], back[1], back[2]
+    back[0], back[1], back[2] = right[2], right[5], right[8]
+    right[2], right[5], right[8] = t1, t2, t3
+
+    t = down[0]
+    down[0] = down[3]
+    down[3] = down[6]
+    down[6] = down[7]
+    down[7] = down[8]
+    down[8] = down[5]
+    down[5] = down[2]
+    down[2] = down[1]
+    down[1] = t
+
+
+def F():
+    t1, t2, t3 = up[6], up[7], up[8]
+    up[6], up[7], up[8] = left[6], left[7], left[8]
+    left[6], left[7], left[8] = down[2], down[1], down[0]
+    down[2], down[1], down[0] = right[6], right[7], right[8]
+    right[6], right[7], right[8] = t1, t2, t3
+
+    t = front[0]
+    front[0] = front[3]
+    front[3] = front[6]
+    front[6] = front[7]
+    front[7] = front[8]
+    front[8] = front[5]
+    front[5] = front[2]
+    front[2] = front[1]
+    front[1] = t
+
+
+def B():
+    t1, t2, t3 = up[0], up[1], up[2]
+    up[0], up[1], up[2] = right[0], right[1], right[2]
+    right[0], right[1], right[2] = down[8], down[7], down[6]
+    down[8], down[7], down[6] = left[0], left[1], left[2]
+    left[0], left[1], left[2] = t1, t2, t3
+
+    t = back[0]
+    back[0] = back[3]
+    back[3] = back[6]
+    back[6] = back[7]
+    back[7] = back[8]
+    back[8] = back[5]
+    back[5] = back[2]
+    back[2] = back[1]
+    back[1] = t
+
 
 
 display()
-start = timer()
+#ri, li, ui, fi, bi, di
+# D()
+# B()
+# F()
+# U()
+# L()
+# R()
 R()
-end = timer()
-print(end-start)
-display()
-L()
+
 display()
 
 # Solve White Corners
